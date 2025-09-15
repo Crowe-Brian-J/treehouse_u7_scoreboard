d0537206afc71ba7a21079f7ab629c07fa5779ba
Brian Crowe <brian.j.crowe@protonmail.com>
2025-09-15 01:03:12 -0400

    refactor Player.js to import new Icon.js component

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,8 +1,9 @@
 import React, { memo } from 'react'
 import PropTypes from 'prop-types'
 
-// import counter component
+// import counter & icon components
 import Counter from './Counter'
+import Icon from './Icon'
 
 const Player = ({ name, score, id, highScore, removePlayer, changeScore }) => {
   return (
@@ -11,22 +12,7 @@ const Player = ({ name, score, id, highScore, removePlayer, changeScore }) => {
         <button className="remove-player" onClick={() => removePlayer(id)}>
           ✖
         </button>
-        <svg
-          className={
-            score === highScore && highScore > 0 ? 'is-high-score' : ''
-          }
-          viewBox="0 0 44 35"
-        >
-          <path
-            d="M26.7616 10.6207L21.8192 0L16.9973 10.5603C15.3699 14.1207 10.9096 15.2672 7.77534 12.9741L0 7.24138L6.56986 28.8448H37.0685L43.5781 7.72414L35.7425 13.0948C32.6685 15.2672 28.3288 14.0603 26.7616 10.6207Z"
-            transform="translate(0 0.301727)"
-          />
-          <rect
-            width="30.4986"
-            height="3.07759"
-            transform="translate(6.56987 31.5603)"
-          />
-        </svg>
+        <Icon score={score} highScore={highScore} />
         {name}
       </span>
 
@@ -47,6 +33,7 @@ Player.propTypes = {
   name: PropTypes.string.isRequired,
   score: PropTypes.number.isRequired,
   id: PropTypes.number,
+  highScore: PropTypes.number.isRequired,
   removePlayer: PropTypes.func,
   changeScore: PropTypes.func
 }
